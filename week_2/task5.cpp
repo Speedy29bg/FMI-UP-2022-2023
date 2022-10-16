ee6308a69edf3e63c7d7c1c77d01c6bfd7f70685
georgi-iliev <georgi_rumenov@mail.bg>
2022-10-16 11:47:37 +0300

    changes

--- week_2/task5.cpp
+++ week_2/task5.cpp
@@ -5,18 +5,29 @@
 -  уравнението ax2 + bx + c = 0 има реални корени
 -  точка се намира извън фигурата тип “венец” с център (0, 0) и радиуси 2 и 8
 */
+bool condition1(int n) {
+    return (n % 3 == 0 && n % 5 == 0) || (n % 2 == 0 && n % 7 == 0);
+}
+
+bool condition2(int a, int b, int c) {
+    int d = b * b - 4 * a * c;
+    return d >= 0;
+}
+
+bool condition3(int x, int y) {
+    return (x * x + y * y > 4 && x * x + y * y < 64);
+}
 #include <iostream>
 int main(){
     int n;
     std::cin >> n;
-    bool flag = false;
-    if(n % 3 == 0 && n % 5 == 0 || n % 2 == 0 && n % 7 == 0){
-        flag = true;
-    }
-    if(flag){
-        std::cout << "True" << std::endl;
-    }else{
-        std::cout << "False" << std::endl;
-    }
-    
+    std::cout << condition1(n) << std::endl;
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::cout << condition2(a, b, c) << std::endl;
+
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << condition3(x, y) << std::endl;
+    return 0;
 }
