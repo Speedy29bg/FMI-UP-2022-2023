e84d551e4a784b34e61834f8b18057fb1954c4a8
georgi-iliev <georgi_rumenov@mail.bg>
2022-10-19 21:20:47 +0300

    changes

---
+++ week_3/task1.cpp
@@ -0,0 +1,28 @@
+//Въведете три реални чилса и ги принтирайте във възходящ ред.
+#include <iostream>
+int main(){
+    double a, b, c;
+    std::cin >> a >> b >> c;
+    if(a > b){
+        if(b > c){
+            std::cout << c << " " << b << " " << a << '\n';
+        }else{
+            if(a > c){
+                std::cout << b << " " << c << " " << a << '\n';
+            }else{
+                std::cout << b << " " << a << " " << c << '\n';
+            }
+        }
+    }else{
+        if(a > c){
+            std::cout << c << " " << a << " " << b << '\n';
+        }else{
+            if(b > c){
+                std::cout << a << " " << c << " " << b << '\n';
+            }else{
+                std::cout << a << " " << b << " " << c << '\n';
+            }
+        }
+    }
+    return 0;
+}
---
+++ week_3/task2.cpp
@@ -0,0 +1,46 @@
+/*
+Напишете програма, която проверява дали 2 триъгълника са еднакви или подобни. 
+Първо проверете дали триъгълниците съществуват.
+*/
+#include<iostream>
+int main(){
+    int a, b, c, a1, b1, c1;
+    std::cin>>a>>b>>c>>a1>>b1>>c1;
+    double diff1 = 0, diff2 = 0, diff3 = 0;
+    if(a + b > c && a + c > b && b + c > a){
+        if(a1 + b1 > c1 && a1 + c1 > b1 && b1 + c1 > a1){
+            if(a == a1 && b == b1 && c == c1){
+                std::cout<<"Triangles are same\n";
+            }
+            else if(a > a1 && b > b1 && c > c1){
+                diff1 = a / a1;
+                diff2 = b / b1;
+                diff3 = c / c1;
+                if (diff1 == diff2 && diff2 == diff3){
+                    std::cout<<"Triangles are similar\n";
+                }
+                else{
+                    std::cout<<"Triangles are not same and not similar";
+                }
+            }
+            else if(a < a1 && b < b1 && c < c1){
+                diff1 = a / a1;
+                diff2 = b / b1;
+                diff3 = c / c1;
+                if (diff1 == diff2 && diff2 == diff3){
+                    std::cout<<"Triangles are similar\n";
+                }
+                else{
+                    std::cout<<"Triangles are not same and not similar";
+                }  
+            }
+        }
+        else{
+            std::cout<<"One of the triangles does not exist!\n";
+        }
+    }
+    else{
+        std::cout<<"One of the triangles does not exist!\n";
+    }
+    return 0;
+}
